5b41c5571cfe7c031d2dc971ef5ef8b293422571
Christoph Suessmeier <mail@suessmeier.net>
2023-11-05 18:24:15 +0100

    Added demo config and sync file

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+// Struct representing a full config file
+type Config struct {
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type (
+	Config struct {
+		SomeStringValue string
+		SomeIntValue    int
+		Something       something
+	}
+
+	something struct {
+		maybe bool
+	}
+)
 
 func main() {
-	fmt.Println("Hello there")
+	configName := "config.toml"
+
+	if _, err := os.Stat(configName); err != nil {
+		fmt.Printf("Unable to find %v\n", configName)
+	}
+
+    var config Config
+    _, err := toml.DecodeFile(configName, &config)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+
+    fmt.Printf("stringValue: %v\nintValue: %v\nmaybe: %v", config.SomeStringValue, config.SomeIntValue, config.Something.maybe)
+
 }
