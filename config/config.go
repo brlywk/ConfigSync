a18a0c6f73d968f7d6b737ffd8fbdafcb6db3256
Christoph Suessmeier <mail@suessmeier.net>
2023-12-06 17:31:20 +0100

    Very basic setup for reading toml config file

--- config/config.go
+++ config/config.go
@@ -1,6 +1,38 @@
 package config
 
-// Struct representing a full config file
-type Config struct {
+type (
+	// Represents content of configuration file
+	Config struct {
+		SyncDirectory      string
+		IncludeSubfolders  bool
+		ExcludeHasPriority bool
+		MappingFile        string
+		CreateDirectories  bool
+		SkipOnError        bool
+		NeverDelete        bool
+		Include            `toml:"include"`
+		Exclude            `toml:"exclude"`
+		Git                `toml:"git"`
+	}
 
-}
+	// Files, directories and patterns to include
+	Include struct {
+		Files       []string
+		Directories []string
+		Patterns    []string
+	}
+
+	// Files, directories and patterns to exclude
+	Exclude struct {
+		Files       []string
+		Directories []string
+		Patterns    []string
+	}
+
+	// Git settings
+	Git struct {
+		Repo             string
+		AlwaysPushOnSync bool
+		CreateBranch     bool
+	}
+)
--- main.go
+++ main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
+	"brlywk/ConfigSync/config"
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/BurntSushi/toml"
 )
 
-type (
-	Config struct {
-		SomeStringValue string
-		SomeIntValue    int
-		Something       something
-	}
-
-	something struct {
-		maybe bool
-	}
-)
-
+// Nobody knows what this function really does...
 func main() {
 	configName := "config.toml"
 
@@ -26,13 +17,19 @@ func main() {
 		fmt.Printf("Unable to find %v\n", configName)
 	}
 
-    var config Config
-    _, err := toml.DecodeFile(configName, &config)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+	var config config.Config
+	_, err := toml.DecodeFile(configName, &config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-    fmt.Printf("stringValue: %v\nintValue: %v\nmaybe: %v", config.SomeStringValue, config.SomeIntValue, config.Something.maybe)
+	c := reflect.TypeOf(&config).Elem()
+
+	for i := 0; i < c.NumField(); i++ {
+		field := c.Field(i)
+		value := reflect.ValueOf(&config).Elem().Field(i).Interface()
+		fmt.Printf("Field: %v\tValue: %v\n", field, value)
+	}
 
 }
